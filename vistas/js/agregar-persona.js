932e137be088dff60fad67829de5a5a80eeb56f9
Jeza077 <jesus.zuniga077@gmail.com>
2020-10-01 14:23:08 -0600

    Cambios en funcion generar contraseñas

--- vistas/js/agregar-persona.js
+++ vistas/js/agregar-persona.js
@@ -29,70 +29,143 @@ $("#tipoPersona").change(function(){
 })
 
 
+
 //** GENERAR CONTRASEÑAS ALEATORIAMENTE */
 $('#generarPassword').on('click', function () {
-    // $longitud = 10; //numero de caracteres
-    // $clave = generarClave($longitud); 
-    $clave = generatePasswordRand(10, 'rand')
-    console.log($clave);
-    $('.passwordGenerado').val($clave);
-    if(/^(\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%.,"'|´;])\S{8,16}$/.test($clave)){
-        console.log('Bien')
-    } else {
-        console.log('mal')
-
-    }
+    var clave = generar_contrasenya(10);
+    $('.passwordGenerado').val(clave);
+    // if(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%+.])\S{8,16}$/.test(clave)){
+    //     console.log('bien');
+    // } else {
+    //     console.log('mal');
+    // }
+    // console.log(clave);
 });
 
-function generatePasswordRand(length,type) {
-    switch(type){
-        case 'num':
-            characters = "0123456789";
-            break;
-        case 'alf':
-            characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            break;
-        case 'rand':
-            //FOR ↓
-            break;
-        default:
-            characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            break;
+//** FUNCION PARA GENERAR CONTRASEÑAS ALEATORIAMENTE */
+function generar_contrasenya(longitud){
+
+    var tamanyo_password				=	longitud;			// definimos el tamaño que tendrá nuestro password
+    var caracteres_conseguidos			=	0;			// contador de los caracteres que hemos conseguido
+    var caracter_temporal				=	'';
+    var array_caracteres				=	new Array();// array para guardar los caracteres de forma temporal
+        
+    for(var i = 0; i < tamanyo_password; i++){		// inicializamos el array con el valor null
+        array_caracteres[i]	=	null;
     }
-    var pass = "";
-    if(/^(\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%.,"'|´;])\S{8,16}$/.test(pass)){
-        return pass;
-    } else {
-        for (i=0; i < length; i++){
-            if(type == 'rand'){
-                pass += String.fromCharCode((Math.floor((Math.random() * 100)) % 94) + 33);
-            }else{
-                pass += characters.charAt(Math.floor(Math.random()*characters.length));   
-            }
+
+    var password_definitivo				=	'';
+    var numero_minimo_letras_minusculas	=	1;			// en ésta y las siguientes variables definimos cuántos 
+    var numero_minimo_letras_mayusculas	=	1;			// caracteres de cada tipo queremos en cada 
+    var numero_minimo_numeros			=	1;
+    var numero_minimo_simbolos			=	1;
+
+    var letras_minusculas_conseguidas 	=	0;
+    var	letras_mayusculas_conseguidas	=	0;
+    var	numeros_conseguidos				=	0;
+    var	simbolos_conseguidos			=	0;
+
+ 
+
+    // función que genera un número aleatorio entre los límites superior e inferior pasados por parámetro
+    function genera_aleatorio(i_numero_inferior, i_numero_superior) {
+        var     i_aleatorio  =   Math.floor((Math.random() * (i_numero_superior - i_numero_inferior + 1)) + i_numero_inferior);
+        return  i_aleatorio;
+    }
+    
+    
+    // función que genera un tipo de caracter en base al tipo que se le pasa por parámetro (mayúscula, minúscula, número, símbolo o aleatorio)
+    function genera_caracter(tipo_de_caracter){
+    // hemos creado una lista de caracteres específica, que además no tiene algunos caracteres como la "i" mayúscula ni la "l" minúscula para evitar errores de transcripción
+    var lista_de_caracteres	=	'!@#$%+.23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz';
+    var caracter_generado	=	'';
+    var valor_inferior		=	0;
+    var valor_superior		=	0;
+    
+    switch (tipo_de_caracter){
+        case 'minúscula':
+            valor_inferior	=	38;
+            valor_superior	=	61;
+            break;
+        case 'mayúscula':
+            valor_inferior	=	14;
+            valor_superior	=	37;
+            break;
+        case 'número':
+            valor_inferior	=	6;
+            valor_superior	=	13;
+            break;
+        case 'símbolo':	
+            valor_inferior	=	0;
+            valor_superior	=	5;
+            break;
+        case 'aleatorio':
+            valor_inferior	=	0;
+            valor_superior	=	61;
+    
+    } // fin del switch
+    
+    caracter_generado	=	lista_de_caracteres.charAt(genera_aleatorio(valor_inferior, valor_superior));
+    return caracter_generado;
+    } // fin de la función genera_caracter()
+    
+    
+    // función que guarda en una posición vacía aleatoria el caracter pasado por parámetro
+    function guarda_caracter_en_posicion_aleatoria(caracter_pasado_por_parametro){
+    var guardado_en_posicion_vacia	=	false;
+    var posicion_en_array			=	0;
+    
+    while(guardado_en_posicion_vacia	!=	true){
+        posicion_en_array	=	genera_aleatorio(0, tamanyo_password-1);	// generamos un aleatorio en el rango del tamaño del password
+    
+        // el array ha sido inicializado con null en sus posiciones. Si es una posición vacía, guardamos el caracter
+        if(array_caracteres[posicion_en_array] == null){
+            array_caracteres[posicion_en_array]	=	caracter_pasado_por_parametro;
+            guardado_en_posicion_vacia			=	true;
         }
-        return pass;
     }
+    }
+    
+    // generamos los distintos tipos de caracteres y los metemos en un password_temporal
+    while (letras_minusculas_conseguidas < numero_minimo_letras_minusculas){
+        caracter_temporal	=	genera_caracter('minúscula');
+        guarda_caracter_en_posicion_aleatoria(caracter_temporal);
+        letras_minusculas_conseguidas++;
+        caracteres_conseguidos++;
+    }
+
+    while (letras_mayusculas_conseguidas < numero_minimo_letras_mayusculas){
+        caracter_temporal	=	genera_caracter('mayúscula');
+        guarda_caracter_en_posicion_aleatoria(caracter_temporal);
+        letras_mayusculas_conseguidas++;
+        caracteres_conseguidos++;
+    }
+
+    while (numeros_conseguidos < numero_minimo_numeros){
+        caracter_temporal	=	genera_caracter('número');
+        guarda_caracter_en_posicion_aleatoria(caracter_temporal);
+        numeros_conseguidos++;
+        caracteres_conseguidos++;
+    }
+
+    while (simbolos_conseguidos < numero_minimo_simbolos){
+        caracter_temporal	=	genera_caracter('símbolo');
+        guarda_caracter_en_posicion_aleatoria(caracter_temporal);
+        simbolos_conseguidos++;
+        caracteres_conseguidos++;
+    }
+
+    // si no hemos generado todos los caracteres que necesitamos, de forma aleatoria añadimos los que nos falten
+    // hasta llegar al tamaño de password que nos interesa
+    while (caracteres_conseguidos < tamanyo_password){
+        caracter_temporal	=	genera_caracter('aleatorio');
+        guarda_caracter_en_posicion_aleatoria(caracter_temporal);
+        caracteres_conseguidos++;
+    }
+
+    // ahora pasamos el contenido del array a la variable password_definitivo
+    for(var i=0; i < array_caracteres.length; i++){
+        password_definitivo	=	password_definitivo + array_caracteres[i];
+    }
+    return password_definitivo;
 }
-
-// /*Función principal | Generador de claves*/
-// function generarClave(long){
-// 	/*caracteres permitidos*/
-// 	let caracteres = "Aa0BbCc1DdEe2FfGgHh3IiJj4KkLl5MmNn6OoPp7QqRr8SsTt9UuVv*#.%!@WwXxYyZz$",
-// 		clave = '',
-// 		numero;
-
-//     /*creacion de clave*/
-//     if(/^(\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%.])\S{8,16}$/.test(clave)){      
-//         return "bien";
-//     } else {
-//         for(let i=0;i<long;i++){
-//             numero = getNumero( 0, caracteres.length );
-//             clave += caracteres.substring( numero, numero + 1 );
-//         }  
-//         return "mal";
-//     }
-// }
-// /*Función para generar un numero aleatorio*/
-// function getNumero(min,max){
-// 	return Math.floor( Math.random() * ( max - min ) ) + min;
-// }
