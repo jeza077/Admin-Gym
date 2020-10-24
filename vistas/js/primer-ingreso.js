6cf2d7ec5a5ad1a6a1dabd7fb4817967441772b4
Jeza077 <jesus.zuniga077@gmail.com>
2020-10-24 15:10:40 -0600

    LISTO diseño de preguntas

--- vistas/js/primer-ingreso.js
+++ vistas/js/primer-ingreso.js
@@ -1,1 +1,31 @@
 
+//** FUNCION PARA PASAR A LA SIGUIENTE PREGUNTA DE SEGURIDAD */
+function togglePregunta1(){
+    var container = document.querySelector('body');
+    container.classList.toggle('pregunta-uno');
+}
+function togglePregunta2(){
+    var container = document.querySelector('body');
+    container.classList.toggle('pregunta-dos');
+}
+function togglePregunta3(){
+    var container = document.querySelector('body');
+    container.classList.toggle('pregunta-tres');
+}
+
+//** FUNCION PARA MOSTRAR CONTENEDOR CAMBIAR CONTRASEÑA */ 
+function togglePassPrimeraVez(){
+    var container = document.querySelector('body');
+    container.classList.toggle('password-primera-vez');
+}
+
+//**AGREGAR BOTONES A CONTENEDOR PREGUNTAS DE SEGURIDAD */
+// Pregunta 1
+$("#pregunta1").append("<a href='javascript:void(0);' class='btn btn-primary float-right' onclick='togglePregunta2();'>Siguiente</a>");
+// Pregunta 2
+$("#pregunta2").append("<a href='javascript:void(0);' class='btn btn-danger float-left' onclick='togglePregunta2();'>Atras</a>");
+$("#pregunta2").append("<a href='javascript:void(0);' class='btn btn-primary float-right' onclick='togglePregunta3();'>Siguiente</a>");
+
+// Pregunta 3
+$("#pregunta3").append("<a href='javascript:void(0);' class='btn btn-danger float-left' onclick='togglePregunta3();'>Atras</a>");
+$("#pregunta3").append("<a href='javascript:void(0);' class='btn btn-primary float-right' onclick='togglePassPrimeraVez();'>Siguiente</a>");
