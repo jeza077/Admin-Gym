0875f2a10ec6a6edb0d15f04fed3eb17cb3c4ee8
HectorFranco12 <67073969+HectorFranco12@users.noreply.github.com>
2020-11-09 17:35:02 -0600

    cambios

--- vistas/modulos/clientes.php
+++ vistas/modulos/clientes.php
@@ -74,12 +74,16 @@
                           } else {
                             echo  '<td>'.$value["tipo_inscripcion"].'</td>';
                           }
-                          
-                      echo  '<td>'.$value["telefono"].'</td>';
 
-                     echo '     
-                          <td>'.$value["fecha_creacion"].'</td>
-                          <td><button class="btn btn-success btn-md">Activado</button></td>
+                      echo  '<td>'.$value["telefono"].'</td>';
+ 
+                          if ($value['fecha_creacion'] == null) {
+                            echo  '<td>**Ninguna**</td>';
+                          } else {
+                            echo   '<td>'.$value["fecha_creacion"].'</td>';                          }
+                          
+                      echo
+                          '<td><button class="btn btn-success btn-md">Activado</button></td>
                           <td>
                             <button class="btn btn-warning btnEditarCliente" id="btnEditar" data-toggle="modal" data-target="#modalEditarCliente" idCliente="'.$value["id_personas"].'"><i class="fas fa-pencil-alt" style="color:#fff"></i></button>
                             <button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
@@ -358,157 +362,162 @@
         </div>
         <div class="modal-body">
           <form role="form" method="post" class="formulario">
-            <!-- <div class="card">
-              <div class="card-body"> -->
-                <div class="form-row">
-                  <div class="form-group col-md-3">
-                    <label for="">Tipo de documento <?php echo $i?></label>
-                    <select class="form-control select2" name="editarTipoDocumento">
-                        <option selected="selected">Seleccionar...</option>
-                        <?php 
-                            $tabla = "tbl_documento";
-                            $item = null;
-                            $valor = null;
-
-                            $preguntas = ControladorUsuarios::ctrMostrar($tabla, $item, $valor);
-
-                            foreach ($preguntas as $key => $value) { ?>
-                                <option value="<?php echo $value['id_documento']?>"><?php echo $value['tipo_documento']?></option>        
-                            <?php 
-                            }
-                        ?>
-                    </select>
-                  </div>
-
-                  <div class="form-group col-md-3">
-                    <label for="identidad">Numero de documento</label>
-                    <input type="text" class="form-control id" name="editarNumeroDocumento" placeholder="Editar Identidad" required>
-                  </div>
-                  <div class="form-group col-md-3">
-                    <label for="nombre">Nombre</label>
-                    <input type="text" class="form-control nombre" name="editarNombre" placeholder="Editar Nombre" required>
-                  </div>
-                  <div class="form-group col-md-3">
-                    <label for="apellido">Apellido</label>
-                    <input type="text" class="form-control apellidos" name="editarApellido" placeholder="Editar Apellidos" required>
-                  </div>
-                </div>
-    
-                <div class="form-row">
-                  <div class="form-group col-md-4">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control email" name="editarEmail" placeholder="Ingrese Email" required>
-                  </div>
-                  <div class="form-group col-md-4">
-                    <label>Teléfono</label>
-                    <input type="text" class="form-control" data-inputmask='"mask": "(999) 9999-9999"' data-mask  name="editarTelefono" placeholder="Editar Telefono" required>
-                  </div>
-                  <div class="form-group col-md-4">
-                    <label>Fecha de nacimiento</label>
-                      <input type="text" class="form-control" data-inputmask-alias="datetime" data-inputmask-inputformat="yyyy/mm/dd" data-mask  name="editarFechaNacimiento" placeholder="Edite Fecha de Nacimiento" required>
-                  </div>
-                </div>
-
-                <div class="form-row">
-                  <div class="form-group col-md-9">
-                    <label for="">Dirección</label>
-                    <input type="text" class="form-control" name="editarDireccion" placeholder="Col. Alameda, calle #2..." required>
-                  </div>
-                
-                  <div class="form-group col-md-3">
-                    <label>Sexo</label>
-                    <select class="form-control select2" name="editarSexo" style="width: 100%;" required>
-                      <option selected="selected">Seleccionar...</option>
-                      <option value="M">Masculino</option>
-                      <option value="F">Femenino</option>
-                    </select>
-                  </div>
-                  <!-- <div class="form-group col-md-4">
-                    <label>Tipo de persona</label>
-                    <select class="form-control select2" style="width: 100%;" id="tipoPersona" name="nuevoTipoPersona" required>
-                      <option selected="selected">Seleccionar...</option>
-                      <option value="empleado">Empleado</option>
-                      <option value="cliente">Cliente</option>
-                    </select>
-                  </div> -->
-                </div>
-
-              <!-- </div>
-            </div> -->
-                    <div class="form-row">
-                      <div class="form-group col-md-3">
-                        <label>Tipo Cliente</label>
-                        <select class="form-control select2 tipoCliente" name="tipoCliente" style="width: 100%;" required>
-                          <option selected="selected">Seleccionar...</option>
-                          <option value="gimnasio">Clientes del gimnasio</option>
-                          <option value="ventas">Cliente de ventas</option>
-                        </select>
-                      </div>
-                    </div>
-                    
-                  <div id="datosClientes">
-                  <div class="form-row">
-                      <div class="form-group col-md-3">
-                          <label>Tipo matricula</label>
-                          <select class="form-control select2" style="width: 100%;" name="nuevaMatricula">
-                            <option selected="selected" value="1">normal</option>
-                            
-                          </select> 
-                      </div>
-                    </div>
-                    <div class="form-row">
-                      <div class="form-group col-md-3">
-                        <label>Promociones</label>
-                        <select class="form-control select2" style="width: 100%;" name="nuevaPromocion">
-                          <option selected="selected">Seleccionar...</option>
-                          
-                            <?php 
-                                $tabla = "tbl_promociones_descuentos";
-                                $item = null;
-                                $valor = null;
-
-                                $matriculas = ControladorClientes::ctrMostrar($tabla, $item, $valor);
-
-                                foreach ($matriculas as $key => $value) { ?>
-                                  <option value="<?php echo $value['id_promociones_descuentos']?>"><?php echo $value['tipo_promociones_descuentos']?></option>        
-                                <?php 
-                                }
-                            ?>
-                        </select> 
-                      </div>
-                    </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-3"> 
-                          <label>Tipo inscripcion</label>
-                          <select class="form-control select2" style="width: 100%;" name="nuevaInscripcion">
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link active" id="datosPersonas" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Datos personales</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link" id="datosClientes" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Datos Cliente</a>
+                </li>
+              </ul>
+              
+              <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="datosclientes">
+                  <div class="container-fluid mt-4">
+                        <div class="form-row">
+                        <div class="form-group col-md-3">
+                          <label for="">Tipo de documento <?php echo $i?></label>
+                          <select class="form-control select2" name="editarTipoDocumento">
                               <option selected="selected">Seleccionar...</option>
                               <?php 
-                                  $tabla = "tbl_inscripcion";
+                                  $tabla = "tbl_documento";
                                   $item = null;
                                   $valor = null;
 
-                                  $inscripciones = ControladorUsuarios::ctrMostrar($tabla, $item, $valor);
+                                  $preguntas = ControladorUsuarios::ctrMostrar($tabla, $item, $valor);
 
-                                  foreach ($inscripciones as $key => $value) { ?>
-                                    <option value="<?php echo $value['id_inscripcion']?>"><?php echo $value['tipo_inscripcion']?></option>        
+                                  foreach ($preguntas as $key => $value) { ?>
+                                      <option value="<?php echo $value['id_documento']?>"><?php echo $value['tipo_documento']?></option>        
                                   <?php 
-                                }
+                                  }
                               ?>
                           </select>
                         </div>
-                    </div>
+
+                        <div class="form-group col-md-3">
+                          <label for="identidad">Numero de documento</label>
+                          <input type="text" class="form-control id" name="editarNumeroDocumento" placeholder="Editar Identidad" required>
+                        </div>
+                        <div class="form-group col-md-3">
+                          <label for="nombre">Nombre</label>
+                          <input type="text" class="form-control nombre" name="editarNombre" placeholder="Editar Nombre" required>
+                        </div>
+                        <div class="form-group col-md-3">
+                          <label for="apellido">Apellido</label>
+                          <input type="text" class="form-control apellidos" name="editarApellido" placeholder="Editar Apellidos" required>
+                        </div>
+                      </div>
+          
+                      <div class="form-row">
+                        <div class="form-group col-md-4">
+                          <label for="email">Email</label>
+                          <input type="email" class="form-control email" name="editarEmail" placeholder="Ingrese Email" required>
+                        </div>
+                        <div class="form-group col-md-4">
+                          <label>Teléfono</label>
+                          <input type="text" class="form-control" data-inputmask='"mask": "(999) 9999-9999"' data-mask  name="editarTelefono" placeholder="Editar Telefono" required>
+                        </div>
+                        <div class="form-group col-md-4">
+                          <label>Fecha de nacimiento</label>
+                            <input type="text" class="form-control" data-inputmask-alias="datetime" data-inputmask-inputformat="yyyy/mm/dd" data-mask  name="editarFechaNacimiento" placeholder="Edite Fecha de Nacimiento" required>
+                        </div>
+                      </div>
+
+                      <div class="form-row">
+                        <div class="form-group col-md-9">
+                          <label for="">Dirección</label>
+                          <input type="text" class="form-control" name="editarDireccion" placeholder="Col. Alameda, calle #2..." required>
+                        </div>
+                      
+                        <div class="form-group col-md-3">
+                          <label>Sexo</label>
+                          <select class="form-control select2" name="editarSexo" style="width: 100%;" required>
+                            <option selected="selected">Seleccionar...</option>
+                            <option value="M">Masculino</option>
+                            <option value="F">Femenino</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="datosCLientes">
+                        <div class="container-fluid mt-4">
+                  
+                          <div class="form-row">
+                            <div class="form-group col-md-3">
+                              <label>Tipo Cliente</label>
+                              <select class="form-control select2 tipoCliente" name="tipoCliente" style="width: 100%;" required>
+                                <option selected="selected">Seleccionar...</option>
+                                <option value="gimnasio">Clientes del gimnasio</option>
+                                <option value="ventas">Cliente de ventas</option>
+                              </select>
+                            </div>
+                          </div>
+                          
+                        <div id="datosClientes">
+                            <div class="form-row">
+                                <div class="form-group col-md-3">
+                                    <label>Tipo matricula</label>
+                                    <select class="form-control select2" style="width: 100%;" name="nuevaMatricula">
+                                      <option selected="selected" value="1">normal</option>
+                                      
+                                    </select> 
+                                </div>
+                              </div>
+                              <div class="form-row">
+                                <div class="form-group col-md-3">
+                                  <label>Promociones</label>
+                                  <select class="form-control select2" style="width: 100%;" name="nuevaPromocion">
+                                    <option selected="selected">Seleccionar...</option>
+                                    
+                                      <?php 
+                                          $tabla = "tbl_promociones_descuentos";
+                                          $item = null;
+                                          $valor = null;
+
+                                          $matriculas = ControladorClientes::ctrMostrar($tabla, $item, $valor);
+
+                                          foreach ($matriculas as $key => $value) { ?>
+                                            <option value="<?php echo $value['id_promociones_descuentos']?>"><?php echo $value['tipo_promociones_descuentos']?></option>        
+                                          <?php 
+                                          }
+                                      ?>
+                                  </select> 
+                                </div>
+                              </div>
+                              <div class="form-row">
+                                  <div class="form-group col-md-3"> 
+                                    <label>Tipo inscripcion</label>
+                                    <select class="form-control select2" style="width: 100%;" name="nuevaInscripcion">
+                                        <option selected="selected">Seleccionar...</option>
+                                        <?php 
+                                            $tabla = "tbl_inscripcion";
+                                            $item = null;
+                                            $valor = null;
+
+                                            $inscripciones = ControladorUsuarios::ctrMostrar($tabla, $item, $valor);
+
+                                            foreach ($inscripciones as $key => $value) { ?>
+                                              <option value="<?php echo $value['id_inscripcion']?>"><?php echo $value['tipo_inscripcion']?></option>        
+                                            <?php 
+                                          }
+                                        ?>
+                                    </select>
+                                  </div>
+                              </div>
+                            </div>
+                        </div>
+                      </div>
+                  
+                      <div class="modal-footer">
+                      <div class="form-group mt-4 float-right">
+                        
+                        <button type="" class="btn btn-primary">Guardar cambios</button>
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Salir</button>
+                      </div>
                   </div>
                 </div>
-            
-                <div class="modal-footer">
-                <div class="form-group mt-4 float-right">
-                  
-                  <button type="" class="btn btn-primary">Guardar cambios</button>
-                  <button type="button" class="btn btn-danger" data-dismiss="modal">Salir</button>
-                </div>
+              </div>
           </form>
         </div>
-
       </div>
     </div>
   </div>
