e7684c48f990c51e26919319a0cb0346bbdfa8e0
Jesus Zuniga <jesus.zuniga077@gmail.com>
2020-11-28 13:45:29 -0600

    Trayendo datos para mostrar en Grafico de ventas

--- vistas/js/ventas.js
+++ vistas/js/ventas.js
@@ -3,14 +3,14 @@
 ======================================--*/
 var pathname = window.location.href;
 
-if(localStorage.getItem("capturarRango") != null && pathname == 'http://localhost/Admin-Gym/administrar-venta'){
+if(localStorage.getItem("capturarRangoVentas") != null && pathname == 'http://localhost/Admin-Gym/administrar-venta'){
 
   $("#daterange-btn span").html('<i class="fa fa-calendar"></i> Rango de fecha');
-  localStorage.removeItem("capturarRango");
+  localStorage.removeItem("capturarRangoVentas");
   localStorage.clear();
   
-} else if(localStorage.getItem("capturarRango") != null) {
-  $("#daterange-btn span").html(localStorage.getItem("capturarRango"));
+} else if(localStorage.getItem("capturarRangoVentas") != null) {
+  $("#daterange-btn span").html(localStorage.getItem("capturarRangoVentas"));
 
 } else {
   $("#daterange-btn span").html('<i class="fa fa-calendar"></i> Rango de fecha');
@@ -392,82 +392,89 @@ function listarProductos(){
 /*=============================================
     DATERANGE PICKER
   ===========================================*/
-$('#daterange-btn').daterangepicker({
-  ranges   : {
-  'Hoy'       : [moment(), moment()],
-  'Ayer'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-  'Últimos 7 días' : [moment().subtract(6, 'days'), moment()],
-  'Últimos 30 días': [moment().subtract(29, 'days'), moment()],
-  'Este mes'  : [moment().startOf('month'), moment().endOf('month')],
-  'Último mes'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-  },
-  startDate: moment(),
-  endDate  : moment()
-  },
-  function (start, end) {
-      $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-    
-      var fechaInicial = start.format('YYYY-MM-DD');
-
-      var fechaFinal = end.format('YYYY-MM-DD');
-
-      var capturarRango = $("#daterange-btn span").html();
-      console.log(capturarRango)
-      localStorage.setItem("capturarRango", capturarRango);
-
-      window.location = "index.php?ruta=administrar-venta&fechaInicial="+fechaInicial+"&fechaFinal="+fechaFinal;
-  }
-)
-  /*=============================================
-         CAPTURAR HOY
-  =============================================*/  
-$(".daterangepicker .ranges li").on("click", function(){
-
-  var textoHoy = $(this).attr("data-range-key");
-  console.log(textoHoy)
-  if(textoHoy == "Hoy"){
-
-    var d = new Date();
-
-    var dia = d.getDate();
-    var mes = d.getMonth()+1;
-    var año = d.getFullYear();
-
-    //   if(mes < 10){
-
-    //       var fechaInicial = año+"-0"+mes+"-"+dia;
-    //       var fechaFinal = año+"-0"+mes+"-"+dia;
-
-    //   }else if(dia < 10){
-
-    //       var fechaInicial = año+"-"+mes+"-0"+dia;
-    //       var fechaFinal = año+"-"+mes+"-0"+dia;
-
-    //   }else if(mes < 10 && dia < 10){
-
-    //       var fechaInicial = año+"-0"+mes+"-0"+dia;
-    //       var fechaFinal = año+"-0"+mes+"-0"+dia;
-
-    //   }else{
-
-    //       var fechaInicial = año+"-"+mes+"-"+dia;
-    //       var fechaFinal = año+"-"+mes+"-"+dia;
-
-    //   }	
-
-    // dia = ("0"+dia).slice(-2);
-    // mes = ("0"+mes).slice(-2);
-
-    var fechaInicial = año+"-"+mes+"-"+dia;
-    var fechaFinal = año+"-"+mes+"-"+dia;	
-
-    localStorage.setItem("capturarRango", "Hoy");
-
-    window.location = "index.php?ruta=administrar-venta&fechaInicial="+fechaInicial+"&fechaFinal="+fechaFinal;
-
-  }
-
+$('#daterange-btn').on('click', function () {  
+  $('.daterangepicker').addClass('reporteVentas');
 })
+datarangeDinamico($('#daterange-btn'), $('#daterange-btn span'), 'capturarRangoVentas', 'administrar-venta');
+
+// $('#daterange-btn').daterangepicker({
+//   ranges   : {
+//   'Hoy'       : [moment(), moment()],
+//   'Ayer'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+//   'Últimos 7 días' : [moment().subtract(6, 'days'), moment()],
+//   'Últimos 30 días': [moment().subtract(29, 'days'), moment()],
+//   'Este mes'  : [moment().startOf('month'), moment().endOf('month')],
+//   'Último mes'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+//   },
+//   startDate: moment(),
+//   endDate  : moment()
+//   },
+//   function (start, end) {
+//       $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+    
+//       var fechaInicial = start.format('YYYY-MM-DD');
+
+//       var fechaFinal = end.format('YYYY-MM-DD');
+
+//       var capturarRango = $("#daterange-btn span").html();
+//       console.log(capturarRango)
+//       localStorage.setItem("capturarRango", capturarRango);
+
+//       window.location = "index.php?ruta=administrar-venta&fechaInicial="+fechaInicial+"&fechaFinal="+fechaFinal;
+//   }
+// )
+/*=============================================
+        CAPTURAR HOY
+=============================================*/  
+datarangeDinamicoHoy(".daterangepicker.reporteVentas .ranges li", 'capturarRangoVentas', 'Hoy', 'administrar-venta');
+
+// $(".daterangepicker .ranges li").on("click", function(){
+
+//   var textoHoy = $(this).attr("data-range-key");
+//   console.log(textoHoy)
+//   if(textoHoy == "Hoy"){
+
+//     var d = new Date();
+
+//     var dia = d.getDate();
+//     var mes = d.getMonth()+1;
+//     var año = d.getFullYear();
+
+//     //   if(mes < 10){
+
+//     //       var fechaInicial = año+"-0"+mes+"-"+dia;
+//     //       var fechaFinal = año+"-0"+mes+"-"+dia;
+
+//     //   }else if(dia < 10){
+
+//     //       var fechaInicial = año+"-"+mes+"-0"+dia;
+//     //       var fechaFinal = año+"-"+mes+"-0"+dia;
+
+//     //   }else if(mes < 10 && dia < 10){
+
+//     //       var fechaInicial = año+"-0"+mes+"-0"+dia;
+//     //       var fechaFinal = año+"-0"+mes+"-0"+dia;
+
+//     //   }else{
+
+//     //       var fechaInicial = año+"-"+mes+"-"+dia;
+//     //       var fechaFinal = año+"-"+mes+"-"+dia;
+
+//     //   }	
+
+//     // dia = ("0"+dia).slice(-2);
+//     // mes = ("0"+mes).slice(-2);
+
+//     var fechaInicial = año+"-"+mes+"-"+dia;
+//     var fechaFinal = año+"-"+mes+"-"+dia;	
+
+//     localStorage.setItem("capturarRango", "Hoy");
+
+//     window.location = "index.php?ruta=administrar-venta&fechaInicial="+fechaInicial+"&fechaFinal="+fechaFinal;
+
+//   }
+
+// })
 
 
 
