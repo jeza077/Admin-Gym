ed3f4746b225c2b495a95d37b20cb2d9303567f7
HectorFranco12 <67073969+HectorFranco12@users.noreply.github.com>
2020-12-04 01:21:22 -0600

    Cambios en editar tipo cliente

--- vistas/modulos/clientes.php
+++ vistas/modulos/clientes.php
@@ -103,11 +103,19 @@
                           }
                     
 
-                      echo
+                      echo '<td></td>';
+
+                          if($value['tipo_cliente'] == "Gimnasio"){
+                            echo '<td><button class="btn btn-warning btnEditarCliente" tipoCliente="'.$value['tipo_cliente'].'" id="btnEditar" data-toggle="modal" data-target="#modalEditarClienteGimnasio" idEditarCliente="'.$value["id_personas"].'"><i class="fas fa-pencil-alt" style="color:#fff"></i></button>
+                            <button class="btn btn-danger btnEliminarCliente" idPersona="'.$value["id_personas"].'"><i class="fas fa-trash-alt"></i></button>';
+                          } else {
+                            echo '<td><button class="btn btn-warning btnEditarCliente" tipoCliente="'.$value['tipo_cliente'].'" id="btnEditar" data-toggle="modal" data-target="#modalEditarClienteVenta" idEditarCliente="'.$value["id_personas"].'"><i class="fas fa-pencil-alt" style="color:#fff"></i></button>
+                            <button class="btn btn-danger btnEliminarCliente" idPersona="'.$value["id_personas"].'"><i class="fas fa-trash-alt"></i></button>';
+                          }
                           '<td>
-                            <button class="btn btn-warning btnEditarCliente" tipoCliente="'.$value['tipo_cliente'].'" id="btnEditar" data-toggle="modal" data-target="#modalEditarCliente" idEditarCliente="'.$value["id_personas"].'"><i class="fas fa-pencil-alt" style="color:#fff"></i></button>
-                            <button class="btn btn-danger btnEliminarCliente" idPersona="'.$value["id_personas"].'"><i class="fas fa-trash-alt"></i></button>
-                          </td>
+                            
+                           
+                          
                         </tr>
                     ';
                    
@@ -361,33 +369,21 @@
   </div>
 
   <!-- =======================================
-           MODAL EDITAR CLIENTE
+           MODAL EDITAR CLIENTE GIMNASIO
   ======================================----->
 
-  <div class="modal fade" id="modalEditarCliente" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal fade" id="modalEditarClienteGimnasio" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
       
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Editar cliente</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Editar Cliente Gimnasio</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
           <form role="form" method="post" class="formulario">
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
-              <li class="nav-item" role="presentation">
-                <a class="nav-link active" id="datosEditarPersona" data-toggle="tab" href="#editarPersona" role="tab" aria-controls="home" aria-selected="true">Datos personales</a>
-              </li>
-              <li class="nav-item tabEditarCliente" id="clientt" role="presentation">
-                <a class="nav-link" id="datosEditarCliente" data-toggle="tab" href="#editarCliente" role="tab" aria-controls="profile" aria-selected="false">Datos Cliente</a>
-              </li>
-            </ul>
-            
-            <div class="tab-content" id="myTabContent">
-              <div class="tab-pane fade show active" id="editarPersona" role="tabpanel" aria-labelledby="datoseditarcliente">
-              
                 <div class="container-fluid mt-4">
                   <div class="form-row">
                     <div class="form-group col-md-3">
@@ -452,29 +448,139 @@
                         <option value="M">Masculino</option>
                         <option value="F">Femenino</option>
                       </select>
-                    </div>
-                          
+                    </div>       
                   </div>
-                  <div class="form-group mt-4 float-right btnDatosGimnasio">
+                </div>
+              
+                <div class="form-group mt-4 float-right">
                   
                   <button type="" class="btn btn-primary">Guardar Cambios</button>
                   <button type="button" class="btn btn-danger" data-dismiss="modal">Salir</button>
                 </div>
+                <?php
+                  $ajustes = null;
+                  $tipoPersona = 'clientes';
+                  $pantalla = 'clientes';
+                  $editarPersona = new ControladorPersonas();
+                  $editarPersona->ctrEditarPersona($ajustes, $tipoPersona, $pantalla);
+                ?>
+         
+          </form>
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+   <!-- =======================================
+           MODAL EDITAR CLIENTE VENTA
+  ======================================----->
+
+  <div class="modal fade" id="modalEditarClienteVenta" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+      
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Editar cliente</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form role="form" method="post" class="formulario">
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+              <li class="nav-item" role="presentation">
+                <a class="nav-link active" id="datosEditarPersona" data-toggle="tab" href="#editarPersona" role="tab" aria-controls="home" aria-selected="true">Datos personales</a>
+              </li>
+              <li class="nav-item tabEditarCliente" id="clientt" role="presentation">
+                <a class="nav-link" id="datosEditarCliente" data-toggle="tab" href="#editarCliente" role="tab" aria-controls="profile" aria-selected="false">Datos Cliente</a>
+              </li>
+            </ul>
+            
+            <div class="tab-content" id="myTabContent">
+              <div class="tab-pane fade show active" id="editarPersona" role="tabpanel" aria-labelledby="datoseditarcliente">
+                <div class="container-fluid mt-4">
+                  <div class="form-row">
+                    <div class="form-group col-md-3">
+                      <label for="">Tipo de documento <?php echo $i?></label>
+                      <select class="form-control select2" name="editarTipoDocumento">
+                          <option value="" id="editarTipoDocumento"></option>
+                          <?php 
+                              $tabla = "tbl_documento";
+                              $item = null;
+                              $valor = null;
+
+                              $preguntas = ControladorUsuarios::ctrMostrar($tabla, $item, $valor);
+
+                              foreach ($preguntas as $key => $value) { ?>
+                                  <option value="<?php echo $value['id_documento']?>"><?php echo $value['tipo_documento']?></option>        
+                              <?php 
+                              }
+                          ?>
+                      </select>
+                    </div>
+
+                    <div class="form-group col-md-3">
+                      <label for="identidad">Numero de documento</label>
+                      <input type="text" class="form-control editarNumeroDocumento" name="editarNumeroDocumento" required>
+                    </div>
+                    <div class="form-group col-md-3">
+                      <label for="nombre">Nombre</label>
+                      <input type="text" class="form-control editarNombre mayus" name="editarNombre" required>
+                      <input type="hidden" id="idEditarCliente" name="idEditarCliente">
+                    </div>
+                    <div class="form-group col-md-3">
+                      <label for="apellido">Apellido</label>
+                      <input type="text" class="form-control editarApellido mayus" name="editarApellido" required>
+                    </div>
+                  </div>
+      
+                  <div class="form-row">
+                    <div class="form-group col-md-4">
+                      <label for="email">Email</label>
+                      <input type="email" class="form-control editarEmail" name="editarEmail" required>
+                    </div>
+                    <div class="form-group col-md-4">
+                      <label>Teléfono</label>
+                      <input type="text" class="form-control editarTelefono" data-inputmask='"mask": "(504) 9999-9999"' data-mask  name="editarTelefono" required>
+                    </div>
+                    <div class="form-group col-md-4">
+                      <label>Fecha de nacimiento</label>
+                        <input type="text" class="form-control editarFechaNacimiento" data-inputmask-alias="datetime" data-inputmask-inputformat="yyyy/mm/dd" data-mask  name="editarFechaNacimiento" required>
+                    </div>
+                  </div>
+
+                  <div class="form-row">
+                    <div class="form-group col-md-9">
+                      <label for="">Dirección</label>
+                      <input type="text" class="form-control editarDireccion mayus" name="editarDireccion" required>
+                    </div>
+                  
+                    <div class="form-group col-md-3">
+                      <label>Sexo</label>
+                      <select class="form-control select2" name="editarSexo" style="width: 100%;" required>
+                        <option value="" id="editarSexo"></option>
+                        <option value="M">Masculino</option>
+                        <option value="F">Femenino</option>
+                      </select>
+                    </div>  
+                  </div>
                 </div>
               </div>
 
               <div class="tab-pane fade" id="editarCliente" role="tabpanel" aria-labelledby="datoseditarCLiente">
                 <div class="container-fluid mt-4">
-                    <div class="form-row">
-                      <div class="form-group col-md-3">
-                        <label>Tipo Cliente</label>
-                        <select class="form-control select2 tipoDeCliente" name="editarTipoCliente" style="width: 100%;" required>
-                          <option value="tipoCl" id="editarTipoCliente"></option>
-                          <option value="Gimnasio">Clientes del gimnasio</option>
-                          <option value="Ventas">Cliente de ventas</option>
-                        </select>
-                      </div>
+                  <div class="form-row">
+                    <div class="form-group col-md-3">
+                      <label>Tipo Cliente</label>
+                      <select class="form-control select2 tipoDeCliente" name="editarTipoCliente" style="width: 100%;" required>
+                        <option value="tipoCl" id="editarTipoCliente"></option>
+                        <option value="Gimnasio">Clientes del gimnasio</option>
+                        <option value="Ventas">Cliente de ventas</option>
+                      </select>
                     </div>
+                  </div>
                     
                   <div id="datosCliente2">
                     <div class="form-row">
@@ -569,7 +675,6 @@
                             <input type="text" class="form-control text-right editarPrecioInscripcion totalInscripcion" name="editarPrecioInscripcion" readonly>
                          </div>
                       </div>
-                      
                     </div>
                     <div class="form-row">
                       <div class="form-group col-md-6 float-right">
