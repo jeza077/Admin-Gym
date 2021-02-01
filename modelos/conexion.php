323fe6a9a806bda6501fb0b7c89d01d2f05371c0
Jesus Zuniga <jesus.zuniga077@gmail.com>
2021-02-01 01:43:55 +0000

    DB cambiada para pruebas

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	public static function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=gym_lr",
+		$link = new PDO("mysql:host=localhost;dbname=gym_la_roca",
 			            "root",
-			            "");
+			            "root136.");
 
 		$link->exec("set names utf8");
 
